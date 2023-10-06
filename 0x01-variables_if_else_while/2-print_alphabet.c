1a1cebe0f3f02c1c0a9d32cd7a75de5d2b4d64e3
kariuki11 <irungukenneth433@gmail.com>
2023-10-06 13:02:27 -0700

    2-print_alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabetic
+ * Return: always (Success)
+ */
+
+int main (void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	putchar(c);
+	putchar('\n');
+	return (0);
+}
