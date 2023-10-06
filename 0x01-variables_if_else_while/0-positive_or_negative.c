92e6398b65e7d6cc160b20f637e0c0f1150ee5fb
kariuki11 <irungukenneth433@gmail.com>
2023-10-06 11:27:45 -0700

    positive negative file

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,24 +13,24 @@
  */
 
 int main(void)
-
 {
 
 	int n;
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 
 	if (n > 0)
 	{
-	printf("%d is positive\n", n)
+		printf("%d is positive\n", n);
 	}
 	else if (n == 0)
 	{
-	printf("%d is zero\n", n)
+		printf("%d is zero\n", n);
 	}
 	else
 	{
-	printf("%d id negative\n", n)
+		printf("%d id negative\n", n);
 	}
 	return (0);
 }
