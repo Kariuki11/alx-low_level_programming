880df84a443d1925ea0d5687ddd80dde892af64f
kariuki11 <irungukenneth433@gmail.com>
2023-10-06 06:09:45 -0700

    the positive negative or zero comment

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+#include <time.h>
+
+#include <stdio.h>
+
+
+
+/**
+ * main - prints if number is positive, zero or negative
+ * Return: Always (Success)
+ */
+
+int main(void)
+
+{
+
+	int;srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+	printf("%d is positive\n", n)
+	}
+	else if n == 0
+	{
+	printf("%d is zero\n", n)
+	}
+	else
+	{
+	printf("%d id negative\n", n)
+	}
+	return (0);
+}
