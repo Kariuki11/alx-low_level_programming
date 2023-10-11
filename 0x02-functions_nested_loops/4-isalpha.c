27d268e25e8cadedb79b57bf366c41ff04b1b47b
kariuki11 <irungukenneth433@gmail.com>
2023-10-11 16:00:38 -0700

    the isalph file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,11 @@
 #include "main.h"
+
 /**
- * _isalpha - Check for alphabetic order.
-*
-* @c: The character to be checked.
- *
- * Return: 1 if c is an alphabetic character, 0 otherwise.
- *
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-	Return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
-
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
