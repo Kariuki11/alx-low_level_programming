882aeffeed9eb79a27d2d9539b1aeeedc9f3f4a8
kariuki11 <irungukenneth433@gmail.com>
2023-10-11 13:11:56 -0700

    print the last digit file

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,12 +8,10 @@
 int print_last_digit(int n)
 {
 	int last_digit;
-	
 	if (n < 0)
 	last_digit = -n % 10;
 	else
-	last_digit = n % 10;
-	
+	last_digit = n % 10;	
 	_putchar('0' + last_digit);
 	return last_digit;
 }
