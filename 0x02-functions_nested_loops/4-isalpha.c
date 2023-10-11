fb53218e8b7ae39fbbb9ff997e4c2e736b01bea2
kariuki11 <irungukenneth433@gmail.com>
2023-10-11 13:35:36 -0700

    the lowercase letters

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _isalpha - Check for alphabetic order.
+*
+* @c: The character to be checked.
+ *
+ * Return: 1 if c is an alphabetic character, 0 otherwise.
+ *
+ */
+int _isalpha(int c)
+{
+	Return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+
+}
