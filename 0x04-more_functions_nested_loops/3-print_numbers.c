f24f26979204bc0e7a4e4e27eafa1ae4f96371b2
kariuki11 <irungukenneth433@gmail.com>
2023-10-12 10:14:51 -0700

    the print numbers file

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_numbers - gives numbers 0 to 9
+ * Return:void
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10 ; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
