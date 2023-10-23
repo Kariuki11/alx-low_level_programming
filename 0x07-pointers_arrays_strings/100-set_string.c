f10a9e3c7ca267c5f14ee73e8d24c351196e9eed
kariuki11 <irungukenneth433@gmail.com>
2023-10-23 13:23:18 -0700

    The second file

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+  * set_string - main function
+  *
+  * @s: Function parameter
+  *
+  * @to: Function parameter
+  *
+  * Return: Always 0.
+  */
+void set_string(char **s, char *to)
+
+{
+	*s = to;
+
+}
