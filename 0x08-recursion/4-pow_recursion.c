835d8e22ca1f272bd0411bcbb1a085efbe53a813
kariuki11 <irungukenneth433@gmail.com>
2023-10-25 13:39:10 -0700

    The pow recursion file

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+  * _pow_recursion - function name
+  * @x: parameter 1
+  * @y: parameter 2
+  * Return: value of a given number passed to the function
+  */
+
+
+int _pow_recursion(int x, int y)
+{
+
+if (y < 0)
+        return (-1);
+
+else if (y == 0)
+        return (1);
+
+else
+        return (x * _pow_recursion(x, (y - 1)));
+
+return (0);
+}
