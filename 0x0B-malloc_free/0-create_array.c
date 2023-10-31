06b1461481d983f404c321f85fcae7ba8330bda3
kariuki11 <irungukenneth433@gmail.com>
2023-10-31 13:14:27 -0700

    The zero task projecyt on malloc

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,46 +1,27 @@
 #include "main.h"
-		
 #include <stdlib.h>
-		
+
 /**
-		
- * create_array - create array of size size and assign char c
-		
- * @size: size of array
-		
- * @c: char to assign
-		
- * Description: creat array of size size and assign char c
-		
- * Return: pointer to array, NULL if fail
-		
- *
-		
- */
-		
+* create_array - creates an array of chars,
+* and initializes it with a specific char.
+* @size: the size of the array
+* @c: initial value
+*
+* Return: a pointer to the array, or NULL if it fails
+*/
 char *create_array(unsigned int size, char c)
-		
 {
-		
-	char *str;
-		
+	char *array;
 	unsigned int i;
-		
 
-		
-	str = malloc(sizeof(char) * size);
-		
-	if (size == 0 || str == NULL)
-		
-		return (NULL);
-		
+	if (size == 0)
+	return (NULL);
+	array = (char *)malloc(sizeof(char) * size);
+	if (array == NULL)
+	return (NULL);
 
-		
 	for (i = 0; i < size; i++)
-		
-		str[i] = c;
-		
-	return (str);
-		
-}
+	array[i] = c;
 
+	return (array);
+}
