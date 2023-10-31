4dc37b3690d3a7de0373097aa97ff639e1b7ca87
kariuki11 <irungukenneth433@gmail.com>
2023-10-31 13:28:29 -0700

    The malloc file

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -1,58 +1,33 @@
-#include <stdio.h>
-		
-#include <stdlib.h>
-		
 #include "main.h"
-		
+#include <stdlib.h>
+
 /**
-		
- * _strdup - duplicate to new memory space location
-		
- * @str: char
-		
- * Return: 0
-		
- */
-		
+* _strdup - returns a pointer to a newly allocated space in memory,
+* which contains a copy of the string given as a parameter.
+* @str: the source string
+*
+* Return: returns a pointer to the duplicated string.
+* It returns NULL if insufficient memory was available
+*/
 char *_strdup(char *str)
-		
 {
-		
-	char *aaa;
-		
-	int i, r = 0;
-		
+	char *copy;
+	int i, len = 0;
 
-		
 	if (str == NULL)
-		
-		return (NULL);
-		
-	i = 0;
-		
-	while (str[i] != '\0')
-		
-		i++;
-		
+	return (NULL);
 
-		
-	aaa = malloc(sizeof(char) * (i + 1));
-		
+	while (str[len] != '\0')
+	len++;
 
-		
-	if (aaa == NULL)
-		
-		return (NULL);
-		
+	copy = (char *)malloc((sizeof(char) * len) + 1);
+	if (copy == NULL)
+	return (NULL);
 
-		
-	for (r = 0; str[r]; r++)
-		
-		aaa[r] = str[r];
-		
 
-		
-	return (aaa);
-		
+	for (i = 0; i < len; i++)
+	copy[i] = str[i];
+	copy[len] = '\0';
+
+	return (copy);
 }
-
--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,81 +1,47 @@
 #include "main.h"
 #include <stdlib.h>
-		
+#include <stdio.h>
+
 /**
-		
- * str_concat - get ends of input and add together for size
-		
- * @s1: input one to concat
- * @s2: input two to concat
- * Return: concat of s1 and s2
-		
- */
-		
+* str_concat - concatenates two strings.
+* @s1: first string
+* @s2: second string
+*
+* Return: a pointer to a newly allocated space in memory which
+* contains the contents of s1, followed by the contents of s2,
+* and null terminated. NULL on failure
+*/
 char *str_concat(char *s1, char *s2)
-		
 {
-		
-	char *conct;
-		
-	int i, ci;
-		
+	int i, j, len1, len2, len;
+	char *result;
 
-		
-	if (s1 == NULL)
-		
-		s1 = "";
-		
-	if (s2 == NULL)
-		
-		s2 = "";
-		
+	len1 = len2 = 0;
 
-		
-		i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
-		i++;
-		
-	while (s2[ci] != '\0')
-		
-		ci++;
-		
-	conct = malloc(sizeof(char) * (i + ci + 1));
-		
-
-		
-	if (conct == NULL)
-		
-		return (NULL);
-		
-	i = ci = 0;
-		
-	while (s1[i] != '\0')
-		
+	if (s1 != NULL)
 	{
-		
-		conct[i] = s1[i];
-		
-		i++;
-		
+	i = 0;
+	while (s1[i++] != '\0')
+	len1++;
 	}
-		
 
-		
-	while (s2[ci] != '\0')
-		
+	if (s2 != NULL)
 	{
-		
-		conct[i] = s2[ci];
-		
-		i++, ci++;
-		
+	i = 0;
+	while (s2[i++] != '\0')
+	len2++;
 	}
-		
-	conct[i] = '\0';
-		
-	return (conct);
-		
+
+	len = len1 + len2;
+	result = (char *)malloc(sizeof(char) * (len + 1));
+	if (result == NULL)
+	return (NULL);
+
+	for (i = 0; i < len1; i++)
+	result[i] = s1[i];
+	for (j = 0; j < len2; j++, i++)
+	result[i] = s2[j];
+	result[len] = '\0';
+
+	return (result);
 }
-
