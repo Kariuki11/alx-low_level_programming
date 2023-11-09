eec427ca406acb7800617642a4753439b1b66a94
kariuki11 <irungukenneth433@gmail.com>
2023-11-09 05:40:02 -0800

    The zero task

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns the same of all the parameters
+ * @n: the number of parameters
+ * @...: variables number of arguments
+ *
+ * Return: the sum of all parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	int sum = 0;
+	unsigned int i;
+
+	if (n == 0
+		return 0;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++) {
+		sum += va_arg(args, int);
+	}
+	va_end(args);
+
+	return sum;
+}
