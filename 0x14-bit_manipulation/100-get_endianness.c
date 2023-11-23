6cce980624c3b045fce878eefb83cae52c2daeb3
kariuki11 <irungukenneth433@gmail.com>
2023-11-23 12:56:22 -0800

    The bit manipulation project

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - prog check the endianness
+ *
+ * Return: 0 if big endian, 1 if little endian
+ */
+int get_endianness(void)
+{
+	int a;
+	char *b;
+
+	a = 1;
+	b = (char *)&a;
+	return (*b);
+}
+
