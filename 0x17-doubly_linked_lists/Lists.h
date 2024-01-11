25ae8d29b0737ba8ff313573b9f109363e637c63
kariuki11 <irungukenneth433@gmail.com>
2024-01-11 10:03:54 -0800

    The doublefile

--- 0x17-doubly_linked_lists/Lists.h
+++ 0x17-doubly_linked_lists/Lists.h
@@ -1,15 +1,17 @@
-#ifndef LISTS_H
-#define LISTS_H
+#ifndef _LISTS_
+#define _LISTS_
+
 #include <stdio.h>
 #include <stdlib.h>
 
 /**
- * struct dlistint_s doubly linked list
+ * struct dlistint_s - doubly linked list
  * @n: integer
  * @prev: points to the previous node
- * @next:points to the next node
+ * @next: points to the next node
  *
- * Description: doubly linked listnode structure
+ * Description: doubly linked list node structure
+ * for Holberton project
  */
 typedef struct dlistint_s
 {
@@ -27,5 +29,4 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index);
-
-#endif /* LISTS_H */
+#endif
